8e2b20f086698bf6432ced28276b8b19c2ed2810
arjthakur <arjthakur@gmail.com>
2020-12-20 17:48:11 +0530

    extra model change

---
+++ Survey.Api/Models/ErrorDetails.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+
+namespace Survey.Api.Models
+{
+    /// <summary>
+    /// Global response handle class
+    /// </summary>
+    public class ErrorDetails
+    {
+        /// <summary>
+        /// Status code of error
+        /// </summary>
+        public int StatusCode { get; set; }
+        /// <summary>
+        /// Details of errors
+        /// </summary>
+        public string Message { get; set; }
+        /// <summary>
+        /// Serialized error object
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
+}
